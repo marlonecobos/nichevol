42071dc20bb527b5008c5e39ea52e401c95befda
Marlon E. Cobos <manubio13@gmail.com>
2019-04-09 22:59:38 -0500

    V 0.1.0 histogram pdfs improved

--- R/histograms_env.R
+++ R/histograms_env.R
@@ -112,19 +112,101 @@ histograms_env <- function(M_folder, M_format = "shp", occ_folder, longitude_col
     ## frecuency of each value in M
     pdf(file = paste0(output_folder, "/Histograms_", names(variables)[i], ".pdf"),
         width = 6, height = 9)
-    par(mfrow = c(5, 2), cex = 0.6, mar = (c(4.5, 4, 3.5, 1) + 0.1))
-    plots <- lapply(1:length(df_layer), function(x){
-      ### actual values
-      hist(as.numeric(names(df_layer[[x]])), main = gsub("_", " ", spnames[x]), xlab = "Variable values")
-      points(as.numeric(names(occ_dfs[[x]])), rep(yoccr[[x]], length(occ_dfs[[x]])), col = "darkgreen", pch = 1)
-      abline(v = limits[x, ], col = col1)
 
-      ### median deviation
-      hist(df_layer[[x]], main = gsub("_", " ", spnames[x]), xlab = "Median deviation")
-      points(occ_dfs[[x]], rep(yocc[[x]], length(occ_dfs[[x]])), col = "darkgreen", pch = 1)
-      limit <- floor(CL_lines * length(df_layer[[x]]) / 100)
-      abline(v = sort(df_layer[[x]])[limit], col = col)
-    })
+    # legend characteristics
+    sym_legend <- c("", "", "Occurrences", paste0(CL_lines, "% Confidence limits"))
+    lin <- c(NA, NA, NA, rep(1, length(CL_lines)))
+    poi <- c(NA, NA, 1, rep(NA, length(CL_lines)))
+    colss <- c(NA, NA, "darkgreen", col)
+
+    # plots
+    if (length(df_layer) > 4) {
+      init_length <- 5
+      sec_length <- length(df_layer)
+
+      # first page
+      layout(mat = cbind(c(1, 2, 4, 6, 8), c(1, 3, 5, 7, 9)))
+      plots <- lapply(1:init_length, function(x){
+        if (x == 1) {
+          par(cex = 0.6, mar = (c(0, 0.5, 3.5, 1.2) + 0.1))
+          plot.new()
+          title(main = paste0("Values and median deviations for variable ", names(variables)[x]))
+          legend("topleft", legend = "Description", cex = 1.1, bty = "n")
+          legend("topleft", legend = c("", "", "  The information presented below helps to visualize the",
+                                       "  distribution of values of the variable in the accessible",
+                                       "  area as well as the species occurrences to facilitate the ",
+                                       "  delimitation of conditions to be used in further analyses."), cex = 0.9, bty = "n")
+
+          legend("topright", legend = "Symbology        ", cex = 1.1, bty = "n")
+          legend("topright", legend = sym_legend, lty = lin, pch = poi, col = colss, cex = 0.9, bty = "n")
+
+        } else {
+          par(cex = 0.6, mar = (c(4.5, 4, 3.5, 1) + 0.1))
+          ### actual values
+          hist(as.numeric(names(df_layer[[x - 1]])), main = gsub("_", " ", spnames[x - 1]), xlab = "Variable values")
+          points(as.numeric(names(occ_dfs[[x - 1]])), rep(yoccr[[x - 1]], length(occ_dfs[[x - 1]])), col = "darkgreen", pch = 1)
+          abline(v = limits[x - 1, ], col = col1)
+
+          ### median deviation
+          hist(df_layer[[x - 1]], main = gsub("_", " ", spnames[x - 1]), xlab = "Median deviation")
+          points(occ_dfs[[x - 1]], rep(yocc[[x - 1]], length(occ_dfs[[x - 1]])), col = "darkgreen", pch = 1)
+          limit <- floor(CL_lines * length(df_layer[[x - 1]]) / 100)
+          abline(v = sort(df_layer[[x - 1]])[limit], col = col)
+        }
+      })
+
+      # next pages
+      par(mfrow = c(5, 2), cex = 0.6, mar = (c(4.5, 4, 3.5, 1) + 0.1))
+      plots <- lapply(5:sec_length, function(x){
+        ### actual values
+        hist(as.numeric(names(df_layer[[x]])), main = gsub("_", " ", spnames[x]), xlab = "Variable values")
+        points(as.numeric(names(occ_dfs[[x]])), rep(yoccr[[x]], length(occ_dfs[[x]])), col = "darkgreen", pch = 1)
+        abline(v = limits[x, ], col = col1)
+
+        ### median deviation
+        hist(df_layer[[x]], main = gsub("_", " ", spnames[x]), xlab = "Median deviation")
+        points(occ_dfs[[x]], rep(yocc[[x]], length(occ_dfs[[x]])), col = "darkgreen", pch = 1)
+        limit <- floor(CL_lines * length(df_layer[[x]]) / 100)
+        abline(v = sort(df_layer[[x]])[limit], col = col)
+      })
+
+    } else {
+      init_length <- length(df_layer) + 1
+      # columns of layout
+      c1 <- c(1, seq(2, (length(df_layer) * 2), 2))
+      c2 <- seq(1, ((length(df_layer) * 2) + 1), 2)
+
+      # first page
+      layout(mat = cbind(c1, c2))
+      plots <- lapply(1:init_length, function(x){
+        if (x == 1) {
+          par(cex = 0.6, mar = (c(0, 0.5, 3.5, 1.2) + 0.1))
+          plot.new()
+          title(main = paste0("Values and median deviations for variable ", variable))
+          legend("topleft", legend = "Description", cex = 1.1, bty = "n")
+          legend("topleft", legend = c("", "", "  The information presented below helps to visualize the",
+                                       "  distribution of values of the variable in the accessible",
+                                       "  area as well as the species occurrences to facilitate the ",
+                                       "  delimitation of conditions to be used in further analyses."), cex = 0.9, bty = "n")
+
+          legend("topright", legend = "Symbology        ", cex = 1.1, bty = "n")
+          legend("topright", legend = sym_legend, lty = lin, pch = poi, col = colss, cex = 0.9, bty = "n")
+
+        } else {
+          par(cex = 0.6, mar = (c(4.5, 4, 3.5, 1) + 0.1))
+          ### actual values
+          hist(as.numeric(names(df_layer[[x - 1]])), main = gsub("_", " ", spnames[x - 1]), xlab = "Variable values")
+          points(as.numeric(names(occ_dfs[[x - 1]])), rep(yoccr[[x - 1]], length(occ_dfs[[x - 1]])), col = "darkgreen", pch = 1)
+          abline(v = limits[x - 1, ], col = col1)
+
+          ### median deviation
+          hist(df_layer[[x - 1]], main = gsub("_", " ", spnames[x - 1]), xlab = "Median deviation")
+          points(occ_dfs[[x - 1]], rep(yocc[[x - 1]], length(occ_dfs[[x - 1]])), col = "darkgreen", pch = 1)
+          limit <- floor(CL_lines * length(df_layer[[x - 1]]) / 100)
+          abline(v = sort(df_layer[[x - 1]])[limit], col = col)
+        }
+      })
+    }
     invisible(dev.off())
 
     cat(i, "of", dim(variables)[3], "variables processed\n")
