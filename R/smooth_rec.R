c98f089fd0248f6a7d74b080c1d7bccc8655a758
hannahlowens <hannah.owens@gmail.com>
2019-10-14 12:23:09 +0200

    Add example for smooth_rec, make bin_par_rec more robust to certain combinations of character codes

--- R/smooth_rec.R
+++ R/smooth_rec.R
@@ -5,6 +5,20 @@
 #'
 #' @importFrom stats smooth
 #'
+#' @examples
+#'
+#' tree <- phytools::pbtree(b = 1, d = 0, n = 5, scale = TRUE,
+#'                          nsim = 1, type = "continuous", set.seed(5));
+#' dataTable <- cbind("241" = rep("1", length(tree$tip.label)),
+#'                    "242" = rep("1", length(tree$tip.label)),
+#'                    "243" = c("1", "1", "0", "0", "0"),
+#'                    "244" = c("1", "1", "0", "0", "0"),
+#'                    "245" = c("1", "?", "0", "0", "0"));
+#' rownames(dataTable) <- tree$tip.label;
+#' treeWdata <- geiger::treedata(tree, dataTable);
+#' parsimonyReconstruction <- bin_par_rec(treeWdata);
+#' smooth_rec(parsimonyReconstruction);
+#'
 #' @export
 
 smooth_rec <- function(whole_rec_table) {
